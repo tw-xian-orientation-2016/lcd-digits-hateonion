01f76d34ec371164a1fa4f4028c208f74c793bde
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-03-02 16:19:33 +0800

    finish printLcd

---
+++ spec/printLcd-test.js
@@ -0,0 +1,15 @@
+describe("printLcd()", function() {
+  describe("it should print a LCD digts", function() {
+    var input = "._. ... ._.\n|_| ..| |.|\n..| ..| |_|";
+    var expectText ="._. ... ._.\n" +
+      "|_| ..| |.|\n" +
+        "..| ..| |_|";
+        it('should print correct content', function() {
+
+          spyOn(console, 'log');
+
+          printLcd(input);
+          expect(console.log).toHaveBeenCalledWith(expectText);
+        });
+  });
+});
